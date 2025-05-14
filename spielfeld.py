b6922b723ad6e73234a06dfe2d1045b4cc1ad8fa
domsauter <159060903+domsauter@users.noreply.github.com>
2025-05-14 11:50:51 +0000

    Klassen Feld und Spielfeld hinzugefügt

---
+++ spielfeld.py
@@ -0,0 +1,15 @@
+class Spielfeld:
+    """"""
+    def __init__(self):
+        pass
+    
+    def startfeld(self):
+        pass
+
+    def spielfeld(self):
+        pass
+
+    def zielfeld(self):
+        pass
+
+    
